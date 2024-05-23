843793929a7e19567bd74a316dc4d941c47d7d56
ゆらり <yurarin.yurarin.yureru@gmail.com>
2024-05-23 09:54:15 +0900

    不要なawaitを消去

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import { useState, useEffect } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [userData, setUserData] = useState("");
   const [userIconData, setIconData] = useState("");
 
   const getAPI = async () => {
-    const fetchData = await fetch("https://yurari.microcms.io/api/v1/profile",
-    {"headers" : { "X-MICROCMS-API-KEY" : "SoWrYdhI38HUcJMQVWqhITOl3bvtBC4XBmMg" }});
+    const fetchData = await fetch("https://yurari.microcms.io/api/v1/profile", {
+      headers: { "X-MICROCMS-API-KEY": "SoWrYdhI38HUcJMQVWqhITOl3bvtBC4XBmMg" },
+    });
     const fetchDataJSON = await fetchData.json();
     console.log(fetchDataJSON);
-    await setUserData(fetchDataJSON);
-    await setIconData(fetchDataJSON.icon)
-  }
+    setUserData(fetchDataJSON);
+    setIconData(fetchDataJSON.icon);
+  };
 
   useEffect(() => {
     getAPI();
-  },[]);
+  }, []);
 
   return (
     <div className="App">
-        {userData.name}
-        {userData.url}
-        {userData.bio}
-        {userIconData.url}
+      {userData.name}
+      {userData.url}
+      {userData.bio}
+      {userIconData.url}
     </div>
   );
 }

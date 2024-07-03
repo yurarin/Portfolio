e04985fb5271d9053cf2ffd58974a721c7197c98
yurari <yurarin.yurarin.yureru@gmail.com>
2024-07-03 18:32:46 +0900

    homeのstyle修正

--- src/App.js
+++ src/App.js
@@ -1,96 +1,98 @@
 import { useState, useEffect } from "react";
-import Header from "./components/Header";
+//import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import { Home, About, Work, Blog, WorkDetail, Article } from "./pagesIndex";
 
 function App() {
-  const [userData, setUserData] = useState("");
-  const [userIconData, setIconData] = useState("");
-  const [userWorkData, setWorkData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const [userData, setUserData] = useState("");
+	const [userIconData, setIconData] = useState("");
+	const [userWorkData, setWorkData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  const getAPI = async () => {
-    const fetchProfileData = await fetch("https://yurari.microcms.io/api/v1/profile", {
-      headers: { "X-MICROCMS-API-KEY": "SoWrYdhI38HUcJMQVWqhITOl3bvtBC4XBmMg" },
-    });
-    const fetchWorkData = await fetch("https://yurari.microcms.io/api/v1/work", {
-      headers: { "X-MICROCMS-API-KEY": "SoWrYdhI38HUcJMQVWqhITOl3bvtBC4XBmMg" },
-    });
-    const fetchProfileDataJSON = await fetchProfileData.json();
-    const fetchWorkDataJSON = await fetchWorkData.json();
-    setUserData(fetchProfileDataJSON);
-    setIconData(fetchProfileDataJSON.icon);
-    setWorkData(fetchWorkDataJSON);
-    setIsLoading(false);
-  };
+	const getAPI = async () => {
+		const fetchProfileData = await fetch("https://yurari.microcms.io/api/v1/profile", {
+			headers: { "X-MICROCMS-API-KEY": "SoWrYdhI38HUcJMQVWqhITOl3bvtBC4XBmMg" },
+		});
+		const fetchWorkData = await fetch("https://yurari.microcms.io/api/v1/work", {
+			headers: { "X-MICROCMS-API-KEY": "SoWrYdhI38HUcJMQVWqhITOl3bvtBC4XBmMg" },
+		});
+		const fetchProfileDataJSON = await fetchProfileData.json();
+		const fetchWorkDataJSON = await fetchWorkData.json();
+		setUserData(fetchProfileDataJSON);
+		setIconData(fetchProfileDataJSON.icon);
+		setWorkData(fetchWorkDataJSON);
+		setIsLoading(false);
+	};
 
-  useEffect(() => {
-    getAPI();
-  }, []);
+	useEffect(() => {
+		getAPI();
+	}, []);
 
-  if (isLoading) {
-    return
-  }
+	if (isLoading) {
+		return
+	}
+	
+	const date = new Date();
+	const year = date.getFullYear();
 
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home 
-              userName={userData.name} 
-              iconUrl={userIconData.url} 
-              bio={userData.bio} 
-            />
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <About
-              userName={userData.name} 
-              url={userData.url} 
-              bioDetail={userData.bio_detail} 
-              iconUrl={userIconData.url} 
-              userData={userData}
-            />
-          }
-        />
-        <Route 
-          path="/work" 
-          element={
-            <Work 
-              works={userWorkData}
-            />
-          } 
-        />
-        <Route 
-          path="/work/:id" 
-          element={
-            <WorkDetail />
-          } 
-        />
-        <Route 
-          path="/blog" 
-          element={
-            <Blog />
-          } 
-        />
-        <Route 
-          path="/blog/:id" 
-          element={
-            <Article />
-          } 
-        />
-      </Routes>
-      {/* userName={userData.name}
-      url={userData.url}
-      bio={userData.bio}
-      iconUrl={userIconData.url} */}
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Home 
+							userName={userData.name} 
+							iconUrl={userIconData.url} 
+							bio={userData.bio} 
+						/>
+					}
+				/>
+				<Route
+					path="/about"
+					element={
+						<About
+							userName={userData.name} 
+							url={userData.url} 
+							bioDetail={userData.bio_detail} 
+							iconUrl={userIconData.url} 
+							userData={userData}
+						/>
+					}
+				/>
+				<Route 
+					path="/work" 
+					element={
+						<Work 
+							works={userWorkData}
+						/>
+					} 
+				/>
+				<Route 
+					path="/work/:id" 
+					element={
+						<WorkDetail />
+					} 
+				/>
+				<Route 
+					path="/blog" 
+					element={
+						<Blog />
+					} 
+				/>
+				<Route 
+					path="/blog/:id" 
+					element={
+						<Article />
+					} 
+				/>
+			</Routes>
+
+			<div className="copyright fadeInD">
+				<p>&copy; {year} yurari</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;

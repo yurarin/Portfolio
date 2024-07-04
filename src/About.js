c42f9ff78e99188d9783103cfc16aeec64aa1585
ゆらり <yurarin.yurarin.yureru@gmail.com>
2024-07-04 22:47:02 +0900

    aboutページの修正

--- src/About.js
+++ src/About.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { CircularProgressbar, buildStyles  } from "react-circular-progressbar";
-import 'react-circular-progressbar/dist/styles.css';
 import { FiGithub } from "react-icons/fi";
 
 const About = (props) => {
@@ -31,36 +29,12 @@ const About = (props) => {
 				<div className="skillBox">
 					<div className="skillItem">
 						<p>HTML & CSS</p>
-						<CircularProgressbar 
-							value={props.userData.code_skill_html_css} 
-							text={`${props.userData.code_skill_html_css}%`} 
-							styles={buildStyles({
-								pathColor: "#e44b23",
-								textColor: '#e44b23'
-							})}
-						/>
 					</div>
 					<div className="skillItem">
 						<p>JavaScript</p>
-						<CircularProgressbar 
-							value={props.userData.code_skill_js} 
-							text={`${props.userData.code_skill_js}%`} 
-							styles={buildStyles({
-								pathColor: "#f1e05a",
-								textColor: '#f1e05a'
-							})}
-						/>
 					</div>
 					<div className="skillItem">
 						<p>React.js</p>
-						<CircularProgressbar 
-							value={props.userData.code_skill_react} 
-							text={`${props.userData.code_skill_react}%`} 
-							styles={buildStyles({
-								pathColor: "#61DBFB",
-								textColor: '#61DBFB'
-							})}
-						/>
 					</div>
 				</div>
 
@@ -68,32 +42,12 @@ const About = (props) => {
 				<div className="skillBox">
 					<div className="skillItem">
 						<p>Node.js</p>
-						<CircularProgressbar 
-							value={props.userData.code_skill_nodejs} 
-							text={`${props.userData.code_skill_nodejs}%`} 
-							styles={buildStyles({
-								pathColor: "#417E38",
-								textColor: '#417E38'
-							})}
-						/>
 					</div>
 					<div className="skillItem">
 						<p>Linux</p>
-						<CircularProgressbar 
-							value={props.userData.infra_skill_linux} 
-							text={`${props.userData.infra_skill_linux}%`} 
-							styles={buildStyles({
-								pathColor: "#000000",
-								textColor: '#000000'
-							})}
-						/>
 					</div>
 					<div className="skillItem">
 						<p>Studio One</p>
-						<CircularProgressbar 
-							value={props.userData.music_skill_stuOne} 
-							text={`${props.userData.music_skill_stuOne}%`} 
-						/>
 					</div>
 				</div>
 			</div>    
